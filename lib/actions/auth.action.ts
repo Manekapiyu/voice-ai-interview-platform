8a7c7c2cefdb3b14e5c17130a5928edb7b66896d
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-09-18 11:08:36 +0530

    modify

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -12,7 +12,7 @@ export async function setSessionCookie(idToken: string) {
 
   // Create session cookie
   const sessionCookie = await auth.createSessionCookie(idToken, {
-    expiresIn:  ONE_WEEK * 1000, // milliseconds
+    expiresIn:  ONE_WEEK * 1000, 
   });
 
   // Set cookie in the browser
